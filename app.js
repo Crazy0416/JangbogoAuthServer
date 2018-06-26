d5223e880ac1954928292ed40ec9203d767176e4
Kim Minho <minmin0416@naver.com>
2018-06-26 19:11:07 +0900

    Add cors middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
+const cors = require('cors');
 
 // sesssion
 const session = require('./modules/sessionConfig');
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // passport setting
 app.use(passport.initialize());
