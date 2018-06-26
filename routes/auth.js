99a442ad468c9016dfc94a37376cb8376622a2a8
Kim Minho <minmin0416@naver.com>
2018-06-26 19:48:52 +0900

    Jenkins test4.

--- routes/auth.js
+++ routes/auth.js
@@ -85,7 +85,7 @@ router.get('/logout', logout);
 
 /* GET auth test */
 function listing(req, res, next) {
-    res.send('jenkins test1');
+    res.send('jenkins test4');
 }
 
 /* POST 로그인 passport local 전략 사용 */
