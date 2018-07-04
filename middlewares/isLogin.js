220caf62ca697f33cca32e73384909b8cdf83ac6
Kim Minho <minmin0416@naver.com>
2018-07-04 10:15:46 +0900

    Middleware: Change isLogin response json. 영문 메세지를 한글로 바꾸었습니다. 클라이언트에서 바로 적용할 수 있도록

--- middlewares/isLogin.js
+++ middlewares/isLogin.js
@@ -2,7 +2,7 @@ module.exports = function isLogin(req, res, next) {
     if (req.session.uid) {
         res.json({
             "success" : false,
-            "msg": "Already login!",
+            "msg": "이미 로그인되어있습니다.",
             "time": Date.now()
         })
     } else {
