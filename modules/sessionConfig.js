f80445a633e7db1fc8013b5df7a9fb952050603d
Kim Minho <minmin0416@naver.com>
2018-07-07 15:48:25 +0900

    Modified: Modified when client join or login, server response auth token to json. 쿠키에 token을 저장하는 방식 대신 로컬 스토리지에 저장하는 방식으로 바꾸었기 때문에 response json에 token 값 반환.

--- modules/sessionConfig.js
+++ modules/sessionConfig.js
@@ -10,11 +10,6 @@ const redisStore = require('connect-redis')(session);
 exports = module.exports = session({
     secret: myEnv.SESSION_SECRET,
     name: 'token',
-    cookie: {
-        secure: false,
-        httpOnly: true,
-        maxAge: 60000
-    },
     store: new redisStore({
         client: redisClient,
         ttl: 60000
