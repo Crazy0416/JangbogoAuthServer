602e90856b7747f3bfcd27cb145d5bb8e5c7bece
Kim Minho <minmin0416@naver.com>
2018-08-03 02:14:16 +0900

    Route: Change main search API if user don't have to login.

--- routes/search.js
+++ routes/search.js
@@ -24,64 +24,60 @@ function roomSearch(req, res, next) {
     memberSchema.findById(req.session._id).exec()
         .then((memberObj) => {
             if(memberObj == null) {
-                console.log(new Date() + " -- GET /search/room User doesn't exist.");
-                res.status(500).json({
-                    "success": false,
-                    "msg": "User doesn't exist",
-                    "time": new Date()
-                })
+                let userAddress = req.body.address.split(' ', 2).join(' ');
+                console.log('find address: ', userAddress);
             } else {
-                let userAddress = memberObj.address;
-                debugger;
-                roomSchema.find({address: userAddress})
-                    .populate({
-                        path: 'memberIds',
-                        model: 'member'
-                    })
-                    .exec()
-                    .then((roomArr) => {
-                        let sendArr = [];
+                let userAddress = memberObj.address.split(' ', 2).join(' ');
+                console.log('find address: ', userAddress);
+            }
+            roomSchema.find({address: {$regex: ".*" + userAddress + ".*"}})
+                .populate({
+                    path: 'memberIds',
+                    model: 'member'
+                })
+                .exec()
+                .then((roomArr) => {
+                    let sendArr = [];
 
-                        for(let i = 0; i < roomArr.length; i++) {
-                            for(let k = 0; k < filterArray.length; k++) {
-                                if(roomArr[i].shoppingType.indexOf(filterArray[k]) !== -1) {
-                                    for(let t = 0; t < roomArr[i].memberIds.length; t++) {
-                                        roomArr[i].memberIds[t].shoppingType = undefined;
-                                        roomArr[i].memberIds[t]._id = undefined;
-                                        roomArr[i].memberIds[t].shoppingType = undefined;
-                                        roomArr[i].memberIds[t].roomIds = undefined;
-                                        roomArr[i].memberIds[t].password = undefined;
-                                        roomArr[i].memberIds[t].address = undefined;
-                                        roomArr[i].memberIds[t].admin = undefined;
-                                        roomArr[i].memberIds[t].salt = undefined;
-                                        roomArr[i].memberIds[t].createOn = undefined;
-                                        roomArr[i].memberIds[t].__v = undefined;
-                                        roomArr[i].memberIds[t].age = undefined;
-                                        roomArr[i].memberIds[t].gender = undefined;
-                                    }
-
-                                    sendArr.push(roomArr[i]);
-                                    break;
+                    for(let i = 0; i < roomArr.length; i++) {
+                        for(let k = 0; k < filterArray.length; k++) {
+                            if(roomArr[i].shoppingType.indexOf(filterArray[k]) !== -1) {
+                                for(let t = 0; t < roomArr[i].memberIds.length; t++) {
+                                    roomArr[i].memberIds[t].shoppingType = undefined;
+                                    roomArr[i].memberIds[t]._id = undefined;
+                                    roomArr[i].memberIds[t].shoppingType = undefined;
+                                    roomArr[i].memberIds[t].roomIds = undefined;
+                                    roomArr[i].memberIds[t].password = undefined;
+                                    roomArr[i].memberIds[t].address = undefined;
+                                    roomArr[i].memberIds[t].admin = undefined;
+                                    roomArr[i].memberIds[t].salt = undefined;
+                                    roomArr[i].memberIds[t].createOn = undefined;
+                                    roomArr[i].memberIds[t].__v = undefined;
+                                    roomArr[i].memberIds[t].age = undefined;
+                                    roomArr[i].memberIds[t].gender = undefined;
                                 }
+
+                                sendArr.push(roomArr[i]);
+                                break;
                             }
                         }
+                    }
 
-                        res.json({
-                            "success": true,
-                            "msg": "User's Recommand Room list",
-                            "time": new Date(),
-                            "data": sendArr
-                        })
+                    res.json({
+                        "success": true,
+                        "msg": "User's Recommand Room list",
+                        "time": new Date(),
+                        "data": sendArr
                     })
-                    .catch((errRF) => {
-                        console.log(new Date() + " -- GET /search/room Room Find ERR=> ", errRF);
-                        res.status(500).json({
-                            "success": false,
-                            "msg": "Room Find Error",
-                            "time": new Date()
-                        })
+                })
+                .catch((errRF) => {
+                    console.log(new Date() + " -- GET /search/room Room Find ERR=> ", errRF);
+                    res.status(500).json({
+                        "success": false,
+                        "msg": "Room Find Error",
+                        "time": new Date()
                     })
-            }
+                })
         })
         .catch((errF) => {
             console.log(new Date() + " -- GET /search/room User Find ERR=> ", errF);
